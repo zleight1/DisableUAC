1f00bcef7d263404a6b4fd44cdf1413391b16e9f
Zachary Leighton <zleighton@hvhc.com>
2015-05-14 11:29:03 -0400

    Initial Setup Of Project

---
+++ DisableUAC/Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace DisableUAC
+{
+    public static class Library
+    {
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                //bad
+            }
+        }
+    }
+}
