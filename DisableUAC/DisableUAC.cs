b03b185700c32ea8460d0fc6e8622459e40ee7c4
Zachary Leighton <zleighton@hvhc.com>
2015-05-14 11:52:44 -0400

    Added timer

--- DisableUAC/DisableUAC.cs
+++ DisableUAC/DisableUAC.cs
@@ -4,14 +4,18 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.AccessControl;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace DisableUAC
 {
     public partial class DisableUAC : ServiceBase
     {
+        private Timer regTimer = null;
+
         public DisableUAC()
         {
             InitializeComponent();
@@ -19,10 +23,22 @@ namespace DisableUAC
 
         protected override void OnStart(string[] args)
         {
+            regTimer = new Timer();
+            this.regTimer.Interval = 10000; //10 seconds for testing
+            //this.regTimer.Interval = 300000; //every 5 minutes
+            this.regTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.regTimer_Tick);
+            this.regTimer.Enabled = true;
+            Library.WriteErrorLog("Disable UAC Service has started.");
+        }
+
+        private void regTimer_Tick(object sender, ElapsedEventArgs e)
+        {
+            Library.WriteErrorLog("Setting registry keys...");
         }
 
         protected override void OnStop()
         {
+            Library.WriteErrorLog("Disable UAC Service has stopped.");
         }
     }
 }
