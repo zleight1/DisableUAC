77ebbe1ac625c43a021e191179b9118ad9470052
Zachary Leighton <zleighton@hvhc.com>
2015-05-14 11:39:48 -0400

    Added logging, need to make more modular however.

--- DisableUAC/Library.cs
+++ DisableUAC/Library.cs
@@ -7,10 +7,20 @@ namespace DisableUAC
     {
         public static void WriteErrorLog(Exception ex)
         {
+            WriteErrorLog(ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+        }
+
+        public static void WriteErrorLog(string message)
+        {
             StreamWriter sw = null;
             try
             {
-
+                //this will write to the log if bad stuff happens
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\DisableUAC.log", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + message);
+                //Don't leave the toilet seat up, there are ladies in the house
+                sw.Flush();
+                sw.Close();
             }
             catch (Exception)
             {
